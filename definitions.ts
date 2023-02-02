e61cb6fb9ddaad8a97390fb618173a4f8627533a
Joshua Lowe <joshualowe1002@gmail.com>
2023-02-02 20:56:15 +0000

    Initial Commit

---
+++ definitions.ts
@@ -0,0 +1,84 @@
+const ml4kidsColor = "#4a4b5f";
+
+Blockly.Blocks["import_mlmodel"] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("from mlmodel import *");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(ml4kidsColor);
+    },
+};
+
+Blockly.Blocks["import_mltext"] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("from mltext import *");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(ml4kidsColor);
+    },
+};
+
+Blockly.Blocks['check_model'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("status"), "status")
+          .appendField(" = checkModel(");
+      this.appendValueInput("parameters")
+          .setCheck(null);
+      this.appendDummyInput()
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(ml4kidsColor);
+    }
+};
+
+Blockly.Blocks['classify_text'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("result"), "result")
+          .appendField(" = classifyText(");
+      this.appendValueInput("parameters")
+          .setCheck(null);
+      this.appendDummyInput()
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(ml4kidsColor);
+    }
+};
+
+Blockly.Blocks['store_text'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("storeText(");
+      this.appendValueInput("parameters")
+          .setCheck(null);
+      this.appendDummyInput()
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(ml4kidsColor);
+    }
+};
+
+Blockly.Blocks['train_model'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("trainModel(");
+      this.appendValueInput("parameters")
+          .setCheck(null);
+      this.appendDummyInput()
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(ml4kidsColor);
+    }
+};
+  
---
+++ generators.ts
@@ -0,0 +1,35 @@
+Blockly.Python["import_mlmodel"] = function() {
+    const code = `import mlmodel\n`;
+    return code;
+};
+
+Blockly.Python["import_mltext"] = function() {
+    const code = `import mltext\n`;
+    return code;
+};
+
+Blockly.Python['check_model'] = function(block) {
+    const status = Blockly.Python.nameDB_.getName(block.getFieldValue("status"), Blockly.VARIABLE_CATEGORY_NAME);
+    const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
+    const code = `${status} = checkModel(${parameters})\n`;
+    return code;
+};
+
+Blockly.Python['classify_text'] = function(block) {
+    const result = Blockly.Python.nameDB_.getName(block.getFieldValue("result"), Blockly.VARIABLE_CATEGORY_NAME);
+    const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
+    const code = `${result} = classifyText(${parameters})\n`;
+    return code;
+};
+
+Blockly.Python['store_text'] = function(block) {
+    const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
+    const code = `storeText(${parameters})\n`;
+    return code;
+};
+
+Blockly.Python['train_model'] = function(block) {
+    const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
+    const code = `trainModel(${parameters})\n`;
+    return code;
+};
